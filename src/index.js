946f347b2ea107235dd2652a74b64229420c1c76
miguelbrieva <miguel.brieva@audiomusica.com>
2019-05-13 11:30:29 -0400

    3. Renderizando elementos.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick() {
+  const element = (
+    <div>
+      <h1>Audiomusica</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+setInterval(tick, 1000);
+
+// It calls ReactDOM.render() every second from a setInterval() callback.
+// In practice, most REact appd only call ReactDOM.render() once.
