2cd87d1e5018be8e7d299400ea8b8c3d341e5adc
miguelbrieva <miguel.brieva@audiomusica.com>
2019-05-16 18:01:17 -0400

    v1 funcionando

--- src/index.js
+++ src/index.js
@@ -33,47 +33,73 @@ const Timer = props => (
   </h1>
 );
 
-const TimeControls = props => (
-  <React.Fragment>
-    <button
-      className='start'
-      onClick={() => props.onControlerClick({ type: 'START' })}
-    >
-      Start
-    </button>
-    <button
-      className='stop'
-      onClick={() => props.onControlerClick({ type: 'STOP' })}
-    >
-      Stop
-    </button>
-    <button
-      className='reset'
-      onClick={() => props.onControlerClick({ type: 'RESET' })}
-    >
-      Reset
-    </button>
-  </React.Fragment>
-);
+// const TimerControls = props => (
+//   <React.Fragment>
+//     <button
+//       className='start'
+//       onClick={() => props.onControlerClick({ type: 'START' })}
+//     >
+//       Start
+//     </button>
+//     <button
+//       className='pause'
+//       onClick={() => props.onControlerClick({ type: 'PAUSE' })}
+//     >
+//       Pause
+//     </button>
+//     <button
+//       className='reset'
+//       onClick={() => props.onControlerClick({ type: 'RESET' })}
+//     >
+//       Reset
+//     </button>
+//   </React.Fragment>
+// );
+
+const TimerControler = props => {
+  const { control, current, children, onControlerClick } = props;
+
+  if (control === current) {
+    return <span>{children}</span>;
+  } else {
+    return (
+      // <button onClick={onControlerClick({ type: control })}>{children}</button>
+      <button
+        onClick={() => {
+          onControlerClick({ type: control });
+        }}
+      >
+        {children}
+      </button>
+    );
+  }
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      min: '0',
-      sec: '0',
+      min: 25,
+      sec: 0,
+      lastSelector: null,
+      currentControler: null,
     };
+    this.tick = null;
   }
 
   timeControl = action => {
-    let tick;
-
     const startTimer = () => {
-      tick = setInterval(
+      this.tick = setInterval(
         x => {
           console.log('state: ', x.state);
           let time = parseInt(x.state.min) * 60 + parseInt(x.state.sec);
           console.log(time);
+
+          if (time <= 1) {
+            clearInterval(this.tick);
+            document.getElementById('alarm').play();
+            this.setState({ currentControler: null });
+          }
 
           time--;
           // conversión:
@@ -86,25 +112,53 @@ class App extends React.Component {
       );
     };
 
-    if (action === 'START') {
-      console.log('partir');
-      startTimer();
-    } else if (action === 'STOP') {
-      console.log('detener', tick); // tick está undefined
-      clearInterval(tick);
+    switch (action) {
+      case 'START':
+        startTimer();
+        this.setState({ currentControler: 'START' });
+        break;
+      case 'PAUSE':
+        clearInterval(this.tick);
+        this.setState({ currentControler: 'PAUSE' });
+        break;
+      case 'RESET':
+        clearInterval(this.tick);
+        this.setState({
+          min: this.state.lastSelector,
+          sec: 0,
+          currentControler: 'RESET',
+        });
+        break;
+      default:
+        break;
     }
   };
 
   handleSelect = select => {
     switch (select.type) {
       case 'POMO':
-        this.setState({ min: 25, sec: 0 });
+        this.setState({
+          min: 25,
+          sec: 0,
+          lastSelector: 25,
+          currentControler: null,
+        });
         break;
       case 'BREAK':
-        this.setState({ min: 5, sec: 0 });
+        this.setState({
+          min: 0,
+          sec: 5,
+          lastSelector: 5,
+          currentControler: null,
+        });
         break;
       case 'LARGE_BREAK':
-        this.setState({ min: 10, sec: 0 });
+        this.setState({
+          min: 10,
+          sec: 0,
+          lastSelector: 10,
+          currentControler: null,
+        });
         break;
       default:
         break;
@@ -116,13 +170,17 @@ class App extends React.Component {
       case 'START':
         console.log(handler);
         this.timeControl('START');
+        this.setState({ currentControler: 'START' });
         break;
-      case 'STOP':
+      case 'PAUSE':
         console.log(handler);
-        this.timeControl('STOP');
+        this.timeControl('PAUSE');
+        this.setState({ currentControler: 'PAUSE' });
         break;
       case 'RESET':
         console.log(handler);
+        this.timeControl('RESET');
+        this.setState({ currentControler: 'RESET' });
         break;
       default:
         break;
@@ -134,7 +192,30 @@ class App extends React.Component {
       <React.Fragment>
         <TimerSelects onSelectorClick={this.handleSelect} />
         <Timer min={this.state.min} sec={this.state.sec} />
-        <TimeControls onControlerClick={this.handleControler} />
+        {/* <TimerControls onControlerClick={this.handleControler} /> */}
+        <div>
+          <TimerControler
+            control='START'
+            current={this.state.currentControler}
+            onControlerClick={this.handleControler}
+          >
+            Start
+          </TimerControler>
+          <TimerControler
+            control='PAUSE'
+            current={this.state.currentControler}
+            onControlerClick={this.handleControler}
+          >
+            Pause
+          </TimerControler>
+          <TimerControler
+            control='RESET'
+            current={this.state.currentControler}
+            onControlerClick={this.handleControler}
+          >
+            Reset
+          </TimerControler>
+        </div>
       </React.Fragment>
     );
   }
